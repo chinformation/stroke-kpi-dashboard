2aebd4cb821a075ef3a473a7565533a7cfb3262f
chinformation <ch.information00@gmail.com>
2025-12-24 11:01:14 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kpi-stroke-dashboard/', // 👈 ชื่อ repo
+  base: '/stroke-kpi-dashboard/',
+  build: {
+    outDir: 'docs'
+  }
 })
